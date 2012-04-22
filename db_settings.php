0a4115a793d89e339509caac87580a094d9e6003
Will Stone <info@flamepad.com>
2012-04-22 12:46:19 +0100

    0.1

--- db_settings.php
+++ db_settings.php
@@ -1,9 +1,9 @@
 <?php
 //Private Key used for hashing stored password
-	$flameblog_private_key				=	'moo you will not guess me, ha ha ha hee';
+	$flameblog_private_key				=	'example phrase';
 //Database settings
 	$flameblog_database_host			=	'localhost';
-	$flameblog_database_username		=	'web235-blogusr';
-	$flameblog_database_password		=	'nfF@Ol9,%*M}';
-	$flameblog_database_name			=	'web235-blogusr';
+	$flameblog_database_username		=	'database';
+	$flameblog_database_password		=	'password';
+	$flameblog_database_name			=	'database_name';
 ?>
